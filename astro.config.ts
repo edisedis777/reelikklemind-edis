68d311b11f529a14c77bc1e1e08fad40fa6a91ea
edis <61211582+edisedis777@users.noreply.github.com>
2025-08-21 09:49:31 +0200

    update sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -34,6 +34,7 @@ import rssContentEnhancer from "./src/integrations/rss-content-enhancer";
 import CSSWriter from "./src/integrations/theme-constants-to-css";
 import createFoldersIfMissing from "./src/integrations/create-folders-if-missing";
 import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
 import config from "./constants-config.json";
 import partytown from "@astrojs/partytown";
 const key_value_from_json = {
@@ -85,9 +86,8 @@ export default defineConfig({
 				forward: ["dataLayer.push"],
 			},
 		}),
-		robotsTxt({
-			sitemapBaseFileName: "sitemap",
-		}),
+		robotsTxt(),
+		sitemap(),
 		rssContentEnhancer(),
 		blocksHtmlCacher(),
 		PublicNotionCopier(),
